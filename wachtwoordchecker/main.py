8a220d1ba7db966b48c6cd1aea50f93aa3fbb982
DayaJansen <d.jansen@ll.gymnasiumarnhem.nl>
2025-05-10 18:06:40 +0200

    Verdergegaan met de wachtwoordchecker

--- wachtwoordchecker/main.py
+++ wachtwoordchecker/main.py
@@ -1,12 +1,40 @@
 import js
+import string
+import re
 from pyscript import display, when
 
-@when("click", "#check-btn")
-def check_password(event):
-    input_value = js.document.getElementById("dutch").value
-    output_div = js.document.getElementById("output")
+@when("click", "#check-btn")                    #Dit geeft aan dat de onderstaande fucnties gelijk uitgevoerd moeten worden zodra de knop 'Check' wordt ingedrukt.
 
-    if input_value == "123":
-        output_div.innerText = "Goed wachtwoord! 🎉"
+def has_sequential_chars(pwd, length=4):        #De functie checkt elk deel van het wachtwoord of er karakters oplopend zijn volgens de ASCII-tabel.
+    for i in range(len(pwd) - length + 1):
+        chunk = pwd[i:i+length]
+        if all(ord(chunk[j]) == ord(chunk[0]) + j for j in range(len(chunk))):
+            return True
+    return False
+
+def check_password(event):              #Nieuwe functie die kijkt of een gegeven wachtwoord ook een goed wachtwoord is
+    input_value = js.document.getElementById("dutch").value     
+    output_div = js.document.getElementById("output")
+    password = input_value
+
+    if len(password) < 8:
+        output_div.innerText = "Een goed wachtwoord heeft minstens 8 tekens, probeer het nog eens!"
+    elif not any(c.isupper() for c in password):                        #Het programma kijkt of er een hoofdletter (isupper) aanwezig is
+        output_div.innerText = "Voeg minstens één hoofdletter toe."
+    elif not any(c.islower() for c in password):                        #Het programma kijkt of er een kleine letter (islower) aanwezig is
+        output_div.innerText = "Voeg minstens één kleine letter toe."
+    elif not any(c.isdigit() for c in password):                        #Het programma kijkt of er een cijfer (isdigit) aanwezig is
+        output_div.innerText = "Voeg minstens één cijfer toe."
+    elif not any(c in string.punctuation for c in password):            #Het programma kijkt of er een speciaal teken (string.punctuation) aanwezig is
+        output_div.innerText = "Voeg minstens één speciaal teken toe."
+    elif re.search(r"(.)\1{2,}", password):                             #Het programma zoekt naar één teken dat minstens 2 keer wordt herhaald
+        output_div.innerText = "Gebruik niet te veel herhaalde tekens achter elkaar (zoals 'aaa' of '111')."
+    elif has_sequential_chars(password):                                #Het programma checkt of er opeenvolgende tekens aanwezig zijn, dit maakt een wachtwoord voorspelbaar
+        output_div.innerText = "Vermijd opeenvolgende tekens zoals 'abcd' of '1234'."
     else:
-        output_div.innerText = "Slecht wachtwoord 😢"
+        output_div.innerText = "Goed wachtwoord 🎉"
+
+
+
+
+
